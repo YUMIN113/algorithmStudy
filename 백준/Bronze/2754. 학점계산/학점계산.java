a8abb3c036be7f6e028027e0e208300d9eab3058
CHOIYUMIN <107908721+YUMIN113@users.noreply.github.com>
2022-12-07 16:58:30 +0900

    [Bronze V] Title: 학점계산, Time: 124 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    String score = br.readLine();
+
+    double result = 69 - score.charAt(0);
+
+    if(score.equals("F")) {
+      System.out.print(0.0);
+      return;
+    }
+
+    if(score.charAt(1) == '+') {
+      result += 0.3;
+    }
+
+    if(score.charAt(1) == '-') {
+      result -= 0.3;
+    }
+
+    System.out.print(result);
+  }
+}
