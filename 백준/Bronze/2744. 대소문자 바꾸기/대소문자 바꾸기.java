b024aaa704096a42ac53207528714ed4bdd42a58
CHOIYUMIN <107908721+YUMIN113@users.noreply.github.com>
2022-12-08 14:42:14 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+
+    String str = br.readLine();
+    int result = 0;
+
+    for(int i = 0; i < str.length(); i++) {
+      int alphabet = str.charAt(i);
+
+      if(alphabet >= 65) {
+        result = alphabet + 32;
+      }
+
+      if(alphabet >= 97) {
+        result = alphabet - 32;
+      }
+
+      sb.append(String.valueOf((char) result));
+
+    }
+
+    System.out.print(sb);
+
+  }
+}
