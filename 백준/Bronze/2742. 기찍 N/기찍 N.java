726a1510db86d9d161f53a235e8aeecfed551bf2
CHOIYUMIN <107908721+YUMIN113@users.noreply.github.com>
2022-12-09 23:15:25 +0900

    [Bronze IV] Title: 기찍 N, Time: 296 ms, Memory: 21172 KB -BaekjoonHub

--- 백준/Bronze/2742. 기찍 N/기찍 N.java
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -1,22 +1,25 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
 
   public static void main(String[] args) throws IOException {
 
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringBuilder sb = new StringBuilder();
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     int n = Integer.parseInt(br.readLine());
 
     while(n > 0) {
 
-      sb.append(n + "\n");
+      bw.write(n + "\n");
       n --;
 
     }
-    System.out.print(sb);
+    bw.flush();
+    bw.close();
   }
 }
