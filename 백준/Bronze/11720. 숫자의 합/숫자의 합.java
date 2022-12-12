b916da1130054b3aedfbd00bae526ce3f9968aef
CHOIYUMIN <107908721+YUMIN113@users.noreply.github.com>
2022-12-12 13:50:30 +0900

    [Bronze IV] Title: 숫자의 합, Time: 124 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -37,7 +37,7 @@ public class Main {
 // import java.io.IOException;
 // import java.io.InputStreamReader;
 
-// public class A11654 {
+// public class Main {
 
 //  public static void main(String[] args) throws IOException {
 
