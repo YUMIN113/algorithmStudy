ae38c3631bca9fd6cd78ab52bf65ac69fe1afb7b
CHOIYUMIN <107908721+YUMIN113@users.noreply.github.com>
2022-12-13 00:04:34 +0900

    [Bronze III] Title: 최댓값, Time: 148 ms, Memory: 16000 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int[] num = new int[9];
+    int[] numLine = new int[9];
+
+    int result = 0;
+    int line = 0;
+
+    for(int i = 0; i < 9; i++) {
+      num[i] = Integer.parseInt(br.readLine());
+      numLine[i] = num[i];
+    }
+
+    for(int i = 0; i < 8; i++) {
+      if(num[i] > num[i + 1] || num[i] == num[i + 1]) {
+        num[i + 1] = num[i];
+        result = num[i + 1];
+        line = i + 1;
+      }
+
+      if(num[i] < num[i + 1]) {
+        num[i] = num[i + 1];
+        result = num[i];
+        line = i + 2;
+      } 
+    }
+
+    for(int i = 0; i < 9; i ++) {
+      if(numLine[i] == result) {
+        line = i + 1;
+      }
+    }
+
+    System.out.print(result + "\n" + line);
+
+  }
+}
