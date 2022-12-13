4c1cb84bdfb9fb90165334c218ebbe736d4b8923
CHOIYUMIN <107908721+YUMIN113@users.noreply.github.com>
2022-12-13 23:31:07 +0900

    [Bronze II] Title: 상수, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -29,3 +29,29 @@ public class Main {
 
   }
 }
+
+// 다른 방법
+
+// StringBuilder 에 reverse() 메서드 포함되어 있다.
+// StringBuilder 타입을 String 으로 변환하기 위해 toString() 사용
+
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.util.StringTokenizer;
+
+// public class Main {
+
+//   public static void main(String[] args) throws IOException {
+
+//     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+//     StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+//     int reverse1 = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+//     int reverse2 = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+
+//     System.out.print(reverse1 > reverse2 ? reverse1 : reverse2);
+
+//   }
+// }
