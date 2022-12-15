d01c21e3abeaa893ffc143ea30ce12a89b668ff6
CHOIYUMIN <107908721+YUMIN113@users.noreply.github.com>
2022-12-15 23:35:30 +0900

    [Bronze II] Title: 문자열 반복, Time: 124 ms, Memory: 14064 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -28,3 +28,38 @@ public class Main {
     System.out.print(sb);
   }
 }
+
+
+// 다른 방법
+// StringTokenizer 대신 split() 사용
+// charAt() 대신 getBytes() 사용
+
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+
+// public class Main {
+
+//   public static void main(String[] args) throws IOException {
+
+//     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//     StringBuilder sb = new StringBuilder();
+
+//     int t = Integer.parseInt(br.readLine());
+
+//     for(int i = 0; i < t; i++) {
+
+//       String[] str = br.readLine().split(" ");
+
+//       int r = Integer.parseInt(str[0]);
+
+//       for(byte val : str[1].getBytes()) {
+//         for(int j = 0; j < r; j++) {
+//           sb.append((char) val);
+//         }
+//       }
+//       sb.append("\n");
+//     }
+//     System.out.print(sb);
+//   }
+// }
