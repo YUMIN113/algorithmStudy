e9add4312b26e7a3d2a6f377831a1f990115ba51
CHOIYUMIN <107908721+YUMIN113@users.noreply.github.com>
2022-12-18 23:57:09 +0900

    [Bronze II] Title: OX퀴즈, Time: 132 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -36,3 +36,38 @@ public class Main {
     System.out.print(sb);
   }
 }
+
+// 다른 방법
+
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+
+// public class Main {
+
+//   public static void main(String[] args) throws IOException {
+
+//     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//     StringBuilder sb = new StringBuilder();
+
+//     int n = Integer.parseInt(br.readLine());
+
+//     for(int i = 0; i < n; i++) {
+
+//       int score = 0;
+//       int sum = 0;
+
+//       for (byte input : br.readLine().getBytes()) {
+
+//         if(input == 'O') {
+//           score++;
+//           sum += score;
+//         } else {
+//           score = 0;
+//         }
+//       }
+//       sb.append(sum).append("\n");
+//     }
+//     System.out.print(sb);
+//   }
+// }
