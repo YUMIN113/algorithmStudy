dc7eee7f27aa2648cf6df87e53d230924d27d1d9
CHOIYUMIN <107908721+YUMIN113@users.noreply.github.com>
2022-12-21 22:20:16 +0900

    [Bronze II] Title: 나머지, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -28,3 +28,31 @@ public class Main {
 
   }
 }
+
+// 다른 방법
+
+// HashSet
+// 중복원소 허용하지 않는다.
+// 순서 개념이 없다. 따라서 정렬이 필요하다면 리스트로 변경 후 가능하다.
+
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.util.HashSet;
+
+// public class Main {
+
+//   public static void main(String[] args) throws IOException {
+
+//     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+//     HashSet<Integer> hashSet = new HashSet<>();
+
+//     for(int i = 0; i < 10; i++) {
+//       hashSet.add(Integer.parseInt(br.readLine()) % 42);
+//     }
+
+//     System.out.print(hashSet.size());
+
+//   }
+// }
