b9d0c0d78bfe8355010dd0b7242b27dd7240e754
CHOIYUMIN <107908721+YUMIN113@users.noreply.github.com>
2023-01-26 19:02:23 +0900

    [Bronze II] Title: 숫자의 개수, Time: 120 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -31,3 +31,38 @@ public class Main {
     }
   }
 }
+
+// 다른 방법
+
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+
+// public class Main {
+
+//   public static void main(String[] args) throws IOException {
+
+//     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+//     int multiplyResult = 1;
+
+//     for(int i = 0; i < 3; i++) {
+//       int num = Integer.parseInt(br.readLine());
+//       multiplyResult *= num;
+//     }
+
+//     int[] arr = new int[10];
+
+//     while(multiplyResult != 0) {
+
+//       arr[multiplyResult % 10] ++; 
+//       multiplyResult /= 10;
+      
+//     }
+
+//     for(int result : arr) {
+//       System.out.println(result);
+      
+//     }
+//   }
+// }
