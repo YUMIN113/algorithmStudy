afac27465d36eab52df932aeeb2d59f71597753d
CHOIYUMIN <cfy113@naver.com>
2023-03-08 22:19:01 +0900

    [Silver V] Title: 소수 찾기, Time: 124 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.java
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.java
@@ -31,3 +31,45 @@ public class Main {
         System.out.println(count);
     }
 }
+
+// 제곱근 사용한 다른 방법
+
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.util.StringTokenizer;
+
+// public class Main {
+//     public static void main(String[] args) throws IOException {
+
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+//         int length = Integer.parseInt(br.readLine());
+//         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+//         int count = 0;
+
+//         while (st.hasMoreTokens()) {
+
+//             boolean isPrime = true;
+
+//             int num = Integer.parseInt(st.nextToken());
+
+//             if (num == 1) {
+//                 continue;
+//             }
+
+//             for (int i = 2; i <= Math.sqrt(num); i++) {
+//                 if (num % i == 0) {
+//                     isPrime = false;
+//                     break;
+//                 }
+//             }
+
+//             if (isPrime == true) {
+//                 count ++;
+//             }
+//         }
+//         System.out.println(count);
+//     }
+// }
