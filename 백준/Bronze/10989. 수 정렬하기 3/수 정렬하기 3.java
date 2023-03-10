a0b0d30dd872c30f0932bcd18814875a00ef97bd
CHOIYUMIN <cfy113@naver.com>
2023-03-10 23:33:52 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1664 ms, Memory: 338772 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// counting sort
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int arrLength = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[10001];
+
+        // index 는 입력 숫자, 값은 해당 숫자의 입력된 개수
+        for (int i = 0; i < arrLength; i++) {
+            arr[Integer.parseInt(br.readLine())]++;
+        }
+
+        // 숫자는 1부터 입력 가능하기 때문에 1부터 시작
+        for (int i = 1; i < arr.length; i++) {
+
+            // i를 중복 개수(arr[i]) 만큼 출력
+            while (arr[i] > 0) {
+                sb.append(i);
+                sb.append("\n");
+
+                arr[i]--;
+            }
+        }
+        System.out.println(sb);
+    }
+}
