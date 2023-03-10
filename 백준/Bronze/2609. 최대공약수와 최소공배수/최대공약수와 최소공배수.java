b44d7d5241831876f538cb4e69d845f1a63cfad3
CHOIYUMIN <cfy113@naver.com>
2023-03-11 00:28:57 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -31,3 +31,36 @@ public class Main {
         return a;
     }
 }
+
+// Recursion Function
+
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.util.StringTokenizer;
+
+// public class Main {
+//     public static void main(String[] args) throws IOException {
+
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+//         int a = Integer.parseInt(st.nextToken());
+//         int b = Integer.parseInt(st.nextToken());
+
+//         int gcd = gcd(a, b);
+//         int lcm = (a * b) / gcd;
+
+//         System.out.println(gcd);
+//         System.out.println(lcm);
+//     }
+
+//     // recursion function
+//     private static int gcd(int a, int b) {
+
+//         if (b == 0) {
+//             return a;
+//         }
+//        return gcd(b, a % b);
+//     }
+// }
