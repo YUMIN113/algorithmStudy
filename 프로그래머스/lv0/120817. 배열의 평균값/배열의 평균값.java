8ed4c895f791e5a44b02fd0256a4d197f4af04ea
CHOIYUMIN <cfy113@naver.com>
2023-03-28 21:50:19 +0900

    [level 0] Title: 배열의 평균값, Time: 2.39 ms, Memory: 66.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
@@ -1,9 +1,6 @@
-import java.util.*;
-
 class Solution {
     public double solution(int[] numbers) {
-        
-        return Arrays.stream(numbers).average().orElse(0);
-        
+        double answer = 0;
+        return answer;
     }
 }
