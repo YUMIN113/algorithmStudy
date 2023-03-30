6d43b54ddde06af8693f3aa39c7fd6c75afba03b
CHOIYUMIN <cfy113@naver.com>
2023-03-30 18:36:02 +0900

    [level 0] Title: 순서쌍의 개수, Time: 1.06 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -1,16 +1,6 @@
-import java.util.stream.IntStream;
-
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int sqrt = (int) Math.sqrt(n);
-        
-        answer = (int) IntStream.rangeClosed(1, sqrt).filter(it -> n % it == 0).count();
-        answer += answer;
-        
-        if(Math.sqrt(n) - sqrt == 0.0) {
-            answer--;
-        }
         return answer;
     }
 }
