038e3e23b529e58f8b8dd763baedb1982bc49bfb
CHOIYUMIN <cfy113@naver.com>
2023-05-06 13:09:04 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.02 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
@@ -1,36 +1,45 @@
-// class Solution {
-//     boolean solution(String s) {
-      
-//         int countP = 0;
-//         int countY = 0;
-        
-//         for(int i = 0; i < s.length(); i++) {
-//             if(s.charAt(i) == 'P' || s.charAt(i) == 'p') {
-//                 countP++;
-//             } else if(s.charAt(i) == 'Y' || s.charAt(i) == 'y') {
-//                 countY++;
-//             }
-//         }
-
-//         return (countP == countY) ? true : false;
-//     }
-// }
-
-
 class Solution {
     boolean solution(String s) {
-        
-        int count = 0;
-        s = s.toUpperCase();
+      
+        int countP = 0;
+        int countY = 0;
         
         for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == 'P') {
-                count++;
-            } else if(s.charAt(i) == 'Y') {
-                count--;
+            if(s.charAt(i) == 'P' || s.charAt(i) == 'p') {
+                countP++;
+            } else if(s.charAt(i) == 'Y' || s.charAt(i) == 'y') {
+                countY++;
             }
         }
 
-        return (count == 0) ? true : false;
+        return (countP == countY) ? true : false;
     }
 }
+
+// 다른 사람의 풀이
+// count++ 와 count-- 로 코드 작성
+// class Solution {
+//     boolean solution(String s) {
+//         int count = 0;
+//         s = s.toUpperCase();
+        
+//         for(int i = 0; i < s.length(); i++) {
+//             if(s.charAt(i) == 'P') {
+//                 count++;
+//             } else if(s.charAt(i) == 'Y') {
+//                 count--;
+//             }
+//         }
+//         return (count == 0) ? true : false;
+//     }
+// }
+
+// 다른 풀이
+// class Solution {
+//     boolean solution(String s) {
+        
+//         s = s.toUpperCase();
+//         return s.chars().filter(it -> it == 'P').count() == s.chars().filter(it -> it == 'Y').count();
+        
+//     }
+// }
