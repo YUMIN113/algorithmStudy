9abd88873c1a7573635604b7e46fa1c3e4373595
CHOIYUMIN <cfy113@naver.com>
2023-05-06 11:36:52 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 1.63 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
+++ 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
@@ -3,7 +3,7 @@ import java.util.stream.IntStream;
 class Solution {
     public int solution(int n) {
        
-        return IntStream.range(1, n).filter(it -> n % it == 1).findFirst().getAsInt();
+        return IntStream.range(2, n).filter(it -> n % it == 1).findFirst().getAsInt();
         
     }
 }
