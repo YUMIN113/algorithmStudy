6d72cffe4dd633152aba8d4e4aefd7fe4e12a648
CHOIYUMIN <cfy113@naver.com>
2023-05-07 23:54:26 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 7.04 ms, Memory: 68.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,23 +1,26 @@
 import java.util.Arrays;
 
-// class Solution {
-//     public String solution(String[] seoul) {
-//         int index = 0;
-        
-//         int len = seoul.length;
-//         for(int i = 0; i < len; i++) {
-//             if(seoul[i].equals("Kim")) {
-//                 index = i;
-//                 break;
-//             }
-//         }
-//         return "김서방은 " + index +"에 있다";
-//     }
-// }
-
 class Solution {
     public String solution(String[] seoul) {
-        int index = Arrays.asList(seoul).indexOf("Kim");
+        int index = 0;
+        
+        int len = seoul.length;
+        for(int i = 0; i < len; i++) {
+            if(seoul[i].equals("Kim")) {
+                index = i;
+                break;
+            }
+        }
         return "김서방은 " + index +"에 있다";
     }
 }
+
+// 다른 사람 풀이
+// 자바 배열에서는 indexOf() 지원하지 않는다.
+
+// class Solution {
+//     public String solution(String[] seoul) {
+//         int index = Arrays.asList(seoul).indexOf("Kim");
+//         return "김서방은 " + index +"에 있다";
+//     }
+// }
