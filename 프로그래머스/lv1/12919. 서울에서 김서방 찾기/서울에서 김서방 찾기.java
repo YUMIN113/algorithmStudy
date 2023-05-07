041919dd1ba0ad4fda5d83e779bd232ee84f032f
CHOIYUMIN <cfy113@naver.com>
2023-05-07 23:52:02 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 7.02 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,16 +1,23 @@
 import java.util.Arrays;
 
+// class Solution {
+//     public String solution(String[] seoul) {
+//         int index = 0;
+        
+//         int len = seoul.length;
+//         for(int i = 0; i < len; i++) {
+//             if(seoul[i].equals("Kim")) {
+//                 index = i;
+//                 break;
+//             }
+//         }
+//         return "김서방은 " + index +"에 있다";
+//     }
+// }
+
 class Solution {
     public String solution(String[] seoul) {
-        int index = 0;
-        
-        int len = seoul.length;
-        for(int i = 0; i < len; i++) {
-            if(seoul[i].equals("Kim")) {
-                index = i;
-                break;
-            }
-        }
+        int index = Arrays.asList(seoul).indexOf("Kim");
         return "김서방은 " + index +"에 있다";
     }
 }
