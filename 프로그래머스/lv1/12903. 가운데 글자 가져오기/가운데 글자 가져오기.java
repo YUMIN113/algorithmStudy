80a13633e942d4f8adfee5828b7aa96e04626735
CHOIYUMIN <cfy113@naver.com>
2023-05-09 23:21:34 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.06 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -1,27 +1,30 @@
-// class Solution {
-//     public String solution(String s) {
-//         StringBuilder sb = new StringBuilder();
-        
-//         if(s.length() % 2 == 0) {
-//             for(int i = (s.length() / 2) - 1; i <= (s.length() / 2); i++) {
-//                 sb.append(s.charAt(i));
-//             } 
-//         } else {
-//               sb.append(s.charAt(s.length() / 2));
-//           }
-//         return String.valueOf(sb);
-//     }
-// }
-
 class Solution {
     public String solution(String s) {
-        String answer = " ";
+        StringBuilder sb = new StringBuilder();
         
         if(s.length() % 2 == 0) {
-            answer = s.substring(((s.length() / 2) - 1), ((s.length() / 2) + 1));
+            for(int i = (s.length() / 2) - 1; i <= (s.length() / 2); i++) {
+                sb.append(s.charAt(i));
+            } 
         } else {
-            answer = s.substring((s.length() / 2), ((s.length() / 2) + 1));
+              sb.append(s.charAt(s.length() / 2));
           }
-        return answer;
+        return String.valueOf(sb);
     }
 }
+
+// 다른 사람 풀이
+// String.substring(int startIndex, int endIndex) 사용
+
+// class Solution {
+//     public String solution(String s) {
+//         String answer = " ";
+        
+//         if(s.length() % 2 == 0) {
+//             answer = s.substring(((s.length() / 2) - 1), ((s.length() / 2) + 1));
+//         } else {
+//             answer = s.substring((s.length() / 2), ((s.length() / 2) + 1));
+//           }
+//         return answer;
+//     }
+// }
