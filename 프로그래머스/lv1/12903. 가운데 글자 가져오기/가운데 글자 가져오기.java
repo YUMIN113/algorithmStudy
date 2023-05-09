1c6ecbed1f7a84210b927db5fd148560bd0da5ea
CHOIYUMIN <cfy113@naver.com>
2023-05-09 23:19:14 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.02 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -1,14 +1,27 @@
+// class Solution {
+//     public String solution(String s) {
+//         StringBuilder sb = new StringBuilder();
+        
+//         if(s.length() % 2 == 0) {
+//             for(int i = (s.length() / 2) - 1; i <= (s.length() / 2); i++) {
+//                 sb.append(s.charAt(i));
+//             } 
+//         } else {
+//               sb.append(s.charAt(s.length() / 2));
+//           }
+//         return String.valueOf(sb);
+//     }
+// }
+
 class Solution {
     public String solution(String s) {
-        StringBuilder sb = new StringBuilder();
+        String answer = " ";
         
         if(s.length() % 2 == 0) {
-            for(int i = (s.length() / 2) - 1; i <= (s.length() / 2); i++) {
-                sb.append(s.charAt(i));
-            } 
+            answer = s.substring(((s.length() / 2) - 1), ((s.length() / 2) + 1));
         } else {
-              sb.append(s.charAt(s.length() / 2));
+            answer = s.substring((s.length() / 2), ((s.length() / 2) + 1));
           }
-        return String.valueOf(sb);
+        return answer;
     }
 }
