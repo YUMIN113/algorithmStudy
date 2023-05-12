147ac432c680850f20895958281413c92c639e63
CHOIYUMIN <cfy113@naver.com>
2023-05-12 22:57:54 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,24 +1,23 @@
-// class Solution {
-//     public int solution(int[] numbers) {
-//         int sum = 0;
-//         int totalSum = ((1 + 9) * 9) / 2;
-        
-//         for(int i = 0; i < numbers.length; i++) {
-//             sum += numbers[i];
-//         }
-//         return totalSum - sum;
-//     }
-// }
-
-// 다른 방법
-
 class Solution {
     public int solution(int[] numbers) {
-        int sum = ((1 + 9) * 9) / 2;
-       
-        for(int value : numbers) {
-            sum -= value;
+        int sum = 0;
+        int totalSum = ((1 + 9) * 9) / 2;
+        
+        for(int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
         }
-        return sum;
+        return totalSum - sum;
     }
 }
+
+// 다른 방법
+// class Solution {
+//     public int solution(int[] numbers) {
+//         int sum = ((1 + 9) * 9) / 2;
+       
+//         for(int value : numbers) {
+//             sum -= value;
+//         }
+//         return sum;
+//     }
+// }
