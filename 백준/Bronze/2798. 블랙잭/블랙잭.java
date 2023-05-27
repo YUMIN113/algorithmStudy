c31b5112a44ab12f0c737d6683efae362b101316
CHOIYUMIN <cfy113@naver.com>
2023-05-27 22:26:30 +0900

    [Bronze II] Title: 블랙잭, Time: 128 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        st = new StringTokenizer(br.readLine(), " ");
+        
+        int cardNum = Integer.parseInt(st.nextToken());
+        int maxNum = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(br.readLine(), " ");
+        
+        int[] arr = new int[cardNum];
+        
+        int i = 0;
+        while(st.hasMoreTokens()) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            i++;
+        }
+        
+        long sum = 0;
+        long answer = 0;
+        
+        for(int j = 0; j < arr.length - 2; j++) {
+            for(int k = j + 1; k < arr.length - 1; k++) {
+                for(int l = k + 1; l < arr.length; l++) {
+                    sum = arr[j] + arr[k] + arr[l];
+                    if(answer < sum && sum <= maxNum) {
+                        answer = sum;
+                    }
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
