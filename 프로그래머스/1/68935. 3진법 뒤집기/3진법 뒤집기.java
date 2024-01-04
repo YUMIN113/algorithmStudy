a83662bb1dbaa46bb6149cb0686cebb11aea6d60
CHOIYUMIN <cfy113@naver.com>
2024-01-04 21:59:23 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.06 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -10,11 +10,10 @@ class Solution {
             list.add(n % 3);
             n = n / 3;
         }
-        
+
         for (int i = 0; i < list.size(); i++) {
             answer += list.get(i) * Math.pow(3, ((list.size() - 1) - i));
         }
-        
         return answer;
     }
 }
