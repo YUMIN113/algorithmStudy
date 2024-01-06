703b913dceedc7488f31642f2e002f960310c271
CHOIYUMIN <cfy113@naver.com>
2024-01-06 19:27:04 +0900

    Update 푸드 파이트 대회.java

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -13,20 +13,3 @@ class Solution {
         return String.valueOf(answer);
     }
 }
-
-// 다른 풀이
-class Solution {
-    public String solution(int[] food) {
-        StringBuilder answer = new StringBuilder();
-        
-        answer.append(0);
-        
-        for (int i = food.length - 1; i > 0; i--) {
-            for (int j = 0; j < food[i] / 2; j++) {
-                answer.insert(0, i);
-                answer.append(i);
-            }
-        }
-        return String.valueOf(answer);
-    }
-}
