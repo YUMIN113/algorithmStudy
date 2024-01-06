77ba3eb8e92e1ebe369bb999b5b088d7db389f63
CHOIYUMIN <cfy113@naver.com>
2024-01-06 17:32:03 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.06 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,20 +1,15 @@
 class Solution {
     public String solution(int[] food) {
-        StringBuilder sb = new StringBuilder();
-        String temp = "";
+        StringBuilder answer = new StringBuilder();
         
-        sb.append(0);
+        answer.append(0);
+        
         for (int i = food.length - 1; i > 0; i--) {
-            int count = food[i] / 2;
-            for (int j = 0; j < count; j++) {
-                sb.append(i);
+            for (int j = 0; j < food[i] / 2; j++) {
+                answer.insert(0, i);
+                answer.append(i);
             }
         }
-        temp = String.valueOf(sb);
-        
-        for (int i = 1; i < temp.length(); i++) {
-            sb.insert(0, temp.charAt(i));
-        }
-        return String.valueOf(sb);
+        return String.valueOf(answer);
     }
 }
